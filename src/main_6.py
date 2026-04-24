c3c9abcd2bb0803a1cc30cee47a8afa7abfa40e2
Polina <kuranovapolina@gmail.com>
2026-04-24 22:20:20 +0300

    update main_6.py

--- src/main_6.py
+++ src/main_6.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import time
+import json
 
 from ultralytics import YOLO
 
@@ -11,11 +12,10 @@ from get_luminosity import get_luminosity
 from detect import detect_and_save
 from distance_by_MVDepthNet import DistanceByMVDepthNet, distance_by_MVDepthNet
 
-from utils import calculate_metrics, get_runtime, calculate_metrics_by_dist, calculate_metrics_by_luminosity
-
+from utils import calculate_metrics, get_runtime, calculate_metrics_by_dist, calculate_metrics_by_luminosity, draw_metrics
 
 def calculate_one_image(cur_id, YOLO_model, DepthMap_model, 
-                        DistanceByMVDepthNet_model = None, 
+                        DistanceByMVDepthNet_model, 
                         left_img_data_folder = 'test_data/left', 
                         right_img_data_folder = 'test_data/right', 
                         calib_data_folder = 'test_data/calib', 
@@ -34,25 +34,21 @@ def calculate_one_image(cur_id, YOLO_model, DepthMap_model,
         target_classes = {0: 'person', 2: 'car'}
     )
 
-    distances = {}
-    times = {}
     luminosity = get_luminosity(left_img_path, boxes)
 
     img_shape = cv2.imread(left_img_path).shape
 
     real_distances = distance_from_real_depth_map(
         DepthMap_model, boxes, img_shape, velodyne_file_path, calibration, grid_size = grid_size)
-    distances["real_distances"] = real_distances
 
-    if DistanceByMVDepthNet_model != None:
-        start = time.perf_counter()
-        distances["MVDepthNet"] = distance_by_MVDepthNet(
-            DistanceByMVDepthNet_model, boxes, calibration, 
-            left_img_path = left_img_path, right_img_path = right_img_path)
-        end = time.perf_counter()
-        times["MVDepthNet"] = end - start
+    start = time.perf_counter()
+    distances_by_MVDepthNet = distance_by_MVDepthNet(
+        DistanceByMVDepthNet_model, boxes, calibration, 
+        left_img_path = left_img_path, right_img_path = right_img_path)
+    end = time.perf_counter()
+    runtime = end - start
 
-    return distances, times, luminosity
+    return distances_by_MVDepthNet, real_distances, luminosity, runtime
 
 if __name__ == "__main__":
     # TODO: try get distanse in the middle of the object or get min value
@@ -60,52 +56,59 @@ if __name__ == "__main__":
 
     YOLO_model = YOLO('model/yolo26m.pt')
     DepthMap_model = DepthMap()
-    # DistanceByMVDepthNet_model = DistanceByMVDepthNet(model_path = 'model/opensource_model.pth.tar') 
-
-    luminosities = []
+    DistanceByMVDepthNet_model = DistanceByMVDepthNet(model_path = 'model/opensource_model.pth.tar') 
 
     real_distances = []
-    distances_by_classic_stereo = []
     distances_by_MVDepthNet = []
+    luminosities = []
+    runtimes = []
 
-    times_by_classic_stereo = []
-    times_by_MVDepthNet = []
+    for cur_id in [0, 1]:
+        img_distances_by_MVDepthNet, img_real_distances, img_luminosities, runtime = calculate_one_image(cur_id, YOLO_model, DepthMap_model, 
+                            DistanceByMVDepthNet_model,
+            left_img_data_folder = '/Users/polinakuranova/uni/master_qualification/master_qualification/test_data/left',
+            right_img_data_folder = '/Users/polinakuranova/uni/master_qualification/master_qualification/test_data/right',
+            calib_data_folder = '/Users/polinakuranova/uni/master_qualification/master_qualification/test_data/calib', 
+            velodyne_data_folder = '/Users/polinakuranova/uni/master_qualification/master_qualification/test_data/velodyne')
 
-    for cur_id in range(10):
-        img_distances, times, luminosity = calculate_one_image(cur_id, YOLO_model, DepthMap_model, 
-                            # DistanceByMVDepthNet_model = DistanceByMVDepthNet_model,
-                            left_img_data_folder = '../kitti_dataset/object_detection_dataset/data_object_image_2/testing/image_2', 
-                            right_img_data_folder = '../kitti_dataset/object_detection_dataset/data_object_image_3/testing/image_3', 
-                            calib_data_folder = '../kitti_dataset/object_detection_dataset/data_object_calib/testing/calib', 
-                            velodyne_data_folder = '../kitti_dataset/object_detection_dataset/data_object_velodyne/testing/velodyne'
-                            # ,
-                            # grid_size = 5
-                            )
-        
-        luminosities.extend(luminosity)
-        
-        real_distances.extend(img_distances["real_distances"])
-        # distances_by_MVDepthNet.extend(img_distances["MVDepthNet"])
-
-        # times_by_MVDepthNet.append(times["MVDepthNet"])
-
-    print("-----\nluminosity: ", luminosities)
+        real_distances.extend(img_real_distances)
+        distances_by_MVDepthNet.extend(img_distances_by_MVDepthNet)
+        luminosities.extend(img_luminosities)
+        runtimes.append(runtime)
 
     print()
-    # print("real_distances:", real_distances)
-    # print("MVDepthNet:", distances_by_MVDepthNet)
 
     print("\t\t\tAbsRel\t\tRMSE\t\tRMSE_log\t\tSqRel\t\tAccurancy")
-    # print("MVDepthNet:", calculate_metrics(np.array(distances_by_MVDepthNet), np.array(real_distances)))
+    metrics = calculate_metrics(np.array(distances_by_MVDepthNet), np.array(real_distances))
+    print("MVDepthNet:", metrics)
 
     print("\n\t\t\tAbsRel\t\tRMSE\t\tRMSE_log\t\tSqRel\t\tAccurancy")
-    # print("MVDepthNet:", calculate_metrics_by_dist(np.array(distances_by_MVDepthNet), np.array(real_distances)))
+    metrics_by_dist = calculate_metrics_by_dist(np.array(distances_by_MVDepthNet), np.array(real_distances))
+    print("MVDepthNet:", json.dumps(metrics_by_dist, indent=4, ensure_ascii=False, sort_keys=True))
+    draw_metrics(metrics_by_dist)
 
     print("\n\t\t\tAbsRel\t\tRMSE\t\tRMSE_log\t\tSqRel\t\tAccurancy")
-    # print("MVDepthNet:", calculate_metrics_by_luminosity(np.array(distances_by_MVDepthNet), np.array(real_distances), np.array(luminosities)))
-  
+    metrics_by_lum = calculate_metrics_by_luminosity(np.array(distances_by_MVDepthNet), np.array(real_distances), np.array(luminosities))
+    print("MVDepthNet:", metrics_by_lum)
+
     print()
-    # print("MVDepthNet:", times_by_MVDepthNet)
 
     print("\t\t\tRuntime")
-    # print("MVDepthNet:", get_runtime(np.array(times_by_MVDepthNet)))
+    runtime, fps = get_runtime(np.array(runtimes))
+    print("MVDepthNet:", (runtime, fps))
+
+    annotations = {
+        "all_metrics": metrics,
+        "metrics_by_dist": metrics_by_dist,
+        "metrics_low_lum": metrics_by_lum[0],
+        "metrics_middle_lum": metrics_by_lum[1],
+        "metrics_max_lum": metrics_by_lum[2],
+        "runtime": runtime,
+        "fps": fps,
+    }
+
+    os.makedirs("metrics", exist_ok=True)
+
+    with open("metrics/exp6.json", "w", encoding="utf-8") as f:
+        json.dump(annotations, f, indent=4, ensure_ascii=False)
+
